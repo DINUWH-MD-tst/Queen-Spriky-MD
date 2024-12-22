8cf3718cfbe4b0ecb02f58f86d5502ffc6441e55
DINUWH-MD-tst <dinuraillwaysecond@gmail.com>
2024-12-22 16:35:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'iu4mkbhJ#7xKYHry5mKHKaeoWugw_wIkwQKrwhNeKYvFgVliKR1Q', // Enter Your Session ID
+    SESSION_ID: process.env.SESSION_ID || 'HuwAiDZK#XpMTS1JtykMw4pP8nrZLx0xjJ3RGc8rt9hsWucNaEOc', // Enter Your Session ID
     MONGODB: process.env.MONGODB || 'mongodb://mongo:aHCBTsUhzVIvadJNEBXYgzCePbwwkAhj@junction.proxy.rlwy.net:51068',// Enter Your MongoDB URL
     OWNER_NUMBER: process.env.OWNER_NUMBER || '94720244981',    // Enter Your Owner Number
 };
